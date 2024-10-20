98af08e27480b4f4ae5c08fda63e01ff7724af29
Olha36 <olhakucheruk36@gmail.com>
2024-10-20 12:08:26 +0200

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path='/home' element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
