2c769221e8596689fc0a60d2fcea96ab787ae21a
Olha36 <olhakucheruk36@gmail.com>
2024-10-20 17:46:42 +0200

    about me section's draft is ready

--- src/components/Card2.tsx
+++ src/components/Card2.tsx
@@ -1,4 +1,5 @@
 import '../css/card.css';
+import img1 from '../img/1_optimized.png';
 export default function Card2() {
   return (
     <div>
@@ -10,28 +11,34 @@ export default function Card2() {
             <h3>Frontend developer</h3>
             <p>
               I am a passionate Software Engineer with a focus on frontend development and over 1.5 years of experience
-              mentoring teenagers in web development. Fluent in English and proficient in five languages, I bring strong
+              mentoring teenagers in web development. Proficient in three languages, I bring strong
               communication skills and a global perspective to my work. I am adept at creating dynamic, user-friendly
               web applications and thrive in collaborative environments where I can continue learning and growing. With
               a keen eye for design and a strong technical foundation, I am eager to contribute to innovative projects
               that make a real impact.
             </p>
           </div>
-          <div className="pics"></div>
+          <div className='pics'>
+            <img src={img1} alt='' />
+          </div>
           <div className='about-right'>
-            <h2>About me</h2>
-            <div className='welcome'></div>
+            <h2>ABOUT ME</h2>
+            <div className='welcome'>
+              <div className='welcome-top'></div>
+              <p>Welcome</p>
+              <div className='welcome-bottom'></div>
+            </div>
           </div>
         </div>
 
         <div className='info-container'>
           <div className='info'>
-            <div className='experience'></div>
-            <div className='education'></div>
-            <div className='languages'></div>
-            <div className='tech-skills'></div>
-            <div className='soft-skills'></div>
-            <div className='certifications'></div>
+            <div className='experience'>experience</div>
+            <div className='education'>education</div>
+            <div className='languages'>languages</div>
+            <div className='tech-skills'>tech-skills</div>
+            <div className='soft-skills'>soft-skills</div>
+            <div className='certifications'>certifications</div>
           </div>
           <div className='recommendations'></div>
         </div>
