3d63e6a92e6fb8a0d807c0bcce1f7585b647c7d5
Olha36 <olhakucheruk36@gmail.com>
2024-10-21 16:43:14 +0200

    add skills elements

--- src/components/Card2.tsx
+++ src/components/Card2.tsx
@@ -1,5 +1,7 @@
 import '../css/card.css';
 import img1 from '../img/1_optimized.png';
+import experience from '../img/avatar-icons/1.png';
+import education from '../img/avatar-icons/2.png';
 export default function Card2() {
   return (
     <div>
@@ -11,11 +13,11 @@ export default function Card2() {
             <h3>Frontend developer</h3>
             <p>
               I am a passionate Software Engineer with a focus on frontend development and over 1.5 years of experience
-              mentoring teenagers in web development. Proficient in three languages, I bring strong
-              communication skills and a global perspective to my work. I am adept at creating dynamic, user-friendly
-              web applications and thrive in collaborative environments where I can continue learning and growing. With
-              a keen eye for design and a strong technical foundation, I am eager to contribute to innovative projects
-              that make a real impact.
+              mentoring teenagers in web development. Proficient in three languages, I bring strong communication skills
+              and a global perspective to my work. I am adept at creating dynamic, user-friendly web applications and
+              thrive in collaborative environments where I can continue learning and growing. With a keen eye for design
+              and a strong technical foundation, I am eager to contribute to innovative projects that make a real
+              impact.
             </p>
           </div>
           <div className='pics'>
@@ -33,12 +35,36 @@ export default function Card2() {
 
         <div className='info-container'>
           <div className='info'>
-            <div className='experience'>experience</div>
-            <div className='education'>education</div>
-            <div className='languages'>languages</div>
-            <div className='tech-skills'>tech-skills</div>
-            <div className='soft-skills'>soft-skills</div>
-            <div className='certifications'>certifications</div>
+            <div className='info-left'>
+              <div className='experience'>
+                <img src={experience} alt='' />
+                <h5>experience</h5>
+              </div>
+              <div className='education'>
+                <img src={education} alt='' />
+                <h5>education</h5>
+              </div>
+            </div>
+            <div className='info-center'>
+              <div className='languages'>
+                <img src={education} alt='' />
+                <h5>languages</h5>
+              </div>
+              <div className='tech-skills'>
+                <img src={education} alt='' />
+                <h5>tech-skills</h5>
+              </div>
+            </div>
+            <div className='info-right'>
+              <div className='soft-skills'>
+                <img src={education} alt='' />
+                <h5>soft-skills</h5>
+              </div>
+              <div className='certifications'>
+                <img src={education} alt='' />
+                <h5>certifications</h5>
+              </div>
+            </div>
           </div>
           <div className='recommendations'></div>
         </div>
