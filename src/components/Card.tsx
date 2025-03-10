64e04811a032cff693e42345dbca2e5e4f49ce5f
olha36 <olhakucheruk36@gmail.com>
2025-03-10 16:49:44 +0200

    add contact info to footer

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,26 +1,27 @@
-import { useNavigate } from 'react-router-dom';
-import '../css/card.css';
-import img1 from '../img/1_optimized.png';
-import certificate from '../img/avatar-icons/certificate.png';
-import education from '../img/avatar-icons/education.png';
-import experience from '../img/avatar-icons/experience.png';
-import languages from '../img/avatar-icons/language.png';
-import softSkills from '../img/avatar-icons/soft-skills.png';
-import techSkills from '../img/avatar-icons/tech-skills.png';
-import cv from '../Olha-Kucheruk-CV.pdf';
-import Certifications from '../pages/Ceritications';
-import Projects from '../pages/Projects';
-import Education from './Education';
-import Experience from './Experience';
-import Header from './Header';
-import Language from './Language';
-import References from './References';
-import Skills from './Skills';
+import { useNavigate } from "react-router-dom";
+import "../css/card.css";
+import img1 from "../img/1_optimized.png";
+import certificate from "../img/avatar-icons/certificate.png";
+import education from "../img/avatar-icons/education.png";
+import experience from "../img/avatar-icons/experience.png";
+import languages from "../img/avatar-icons/language.png";
+import softSkills from "../img/avatar-icons/soft-skills.png";
+import techSkills from "../img/avatar-icons/tech-skills.png";
+import cv from "../Olha-Kucheruk-CV.pdf";
+import Certifications from "../pages/Ceritications";
+import Projects from "../pages/Projects";
+import Education from "./Education";
+import Experience from "./Experience";
+import Header from "./Header";
+import Language from "./Language";
+import References from "./References";
+import Skills from "./Skills";
+import Footer from "./Footer";
 
 export default function Card() {
   const navigate = useNavigate();
   const handleExperienceClick = () => {
-    navigate('/experience');
+    navigate("/experience");
     return (
       <>
         <Experience />
@@ -29,7 +30,7 @@ export default function Card() {
   };
 
   const handleLanguageClick = () => {
-    navigate('/languages');
+    navigate("/languages");
     return (
       <>
         <Language />
@@ -38,7 +39,7 @@ export default function Card() {
   };
 
   const handleProjectsClick = () => {
-    navigate('/projects');
+    navigate("/projects");
     return (
       <>
         <Projects />
@@ -47,7 +48,7 @@ export default function Card() {
   };
 
   const handleEducationClick = () => {
-    navigate('/education');
+    navigate("/education");
     return (
       <>
         <Education />
@@ -56,7 +57,7 @@ export default function Card() {
   };
 
   const handleSkillsClick = () => {
-    navigate('/skills');
+    navigate("/skills");
     return (
       <>
         <Skills />
@@ -65,7 +66,7 @@ export default function Card() {
   };
 
   const handleCertificationClick = () => {
-    navigate('/certifications');
+    navigate("/certifications");
     return (
       <>
         <Certifications />
@@ -74,7 +75,7 @@ export default function Card() {
   };
 
   const handleReferencesClick = () => {
-    navigate('/references');
+    navigate("/references");
     return (
       <>
         <References />
@@ -83,9 +84,9 @@ export default function Card() {
   };
 
   const handleDownloadCV = () => {
-    const link = document.createElement('a');
-    link.href = cv; // Path or URL to your CV file
-    link.download = 'OlhaKucheruk_CV.pdf'; // The file name for the downloaded file
+    const link = document.createElement("a");
+    link.href = cv;
+    link.download = "OlhaKucheruk_CV.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -94,86 +95,110 @@ export default function Card() {
   return (
     <div>
       <div>
-        {/* <div className='card-header'></div> */}
         <Header />
-        <div className='card-title title-container'>
-          <div className='about-left'>
+        <div className="card-title title-container">
+          <div className="about-left">
             <h3>Frontend developer</h3>
-            <p className='description'>
-              I am a Software Engineer with 1.5 years of experience mentoring teens in web development and a proven
-              ability to build dynamic, user-friendly applications. Fluent in three languages, I bring strong
-              communication and a global perspective to every project. With a sharp eye for design and solid technical
-              skills, I thrive in collaborative environments and am eager to drive innovation in impactful projects.
+            <p className="description">
+              I am a Software Engineer with 1.5 years of experience mentoring
+              teens in web development and a proven ability to build dynamic,
+              user-friendly applications. Fluent in three languages, I bring
+              strong communication and a global perspective to every project.
+              With a sharp eye for design and solid technical skills, I thrive
+              in collaborative environments and am eager to drive innovation in
+              impactful projects.
             </p>
-            <div className='flex'>
-              <button className='button button-pink' onClick={handleReferencesClick}>
+            <div className="flex">
+              <button
+                className="button button-pink"
+                onClick={handleReferencesClick}
+              >
                 References
               </button>
-              <button className='button button-pink' onClick={handleDownloadCV}>
+              <button className="button button-pink" onClick={handleDownloadCV}>
                 Download CV
               </button>
             </div>
           </div>
-          <div className='about-right'>
+          <div className="about-right">
             <h3>OLHA KUCHERUK</h3>
-            <div className='pics'>
-              <img src={img1} alt='' />
+            <div className="pics">
+              <img src={img1} alt="" />
             </div>
-            <div className='welcome'>
-              <div className='welcome-top'></div>
+            <div className="welcome">
+              <div className="welcome-top"></div>
               <p>Welcome</p>
-              <div className='welcome-bottom'></div>
+              <div className="welcome-bottom"></div>
             </div>
           </div>
         </div>
 
-        <div className='info-container'>
-          <div className='info'>
-            <div className='info-left'>
-              <div className='experience grid'>
-                <img src={experience} alt='' />
-                <button className='button button-pink' onClick={handleExperienceClick}>
+        <div className="info-container">
+          <div className="info">
+            <div className="info-left">
+              <div className="experience grid">
+                <img src={experience} alt="" />
+                <button
+                  className="button button-pink"
+                  onClick={handleExperienceClick}
+                >
                   experience
                 </button>
               </div>
-              <div className='education grid'>
-                <img src={education} alt='' />
-                <button className='button button-pink' onClick={handleEducationClick}>
+              <div className="education grid">
+                <img src={education} alt="" />
+                <button
+                  className="button button-pink"
+                  onClick={handleEducationClick}
+                >
                   education
                 </button>
               </div>
             </div>
-            <div className='info-center'>
-              <div className='languages grid'>
-                <img src={languages} alt='' />
-                <button className='button button-white' onClick={handleLanguageClick}>
+            <div className="info-center">
+              <div className="languages grid">
+                <img src={languages} alt="" />
+                <button
+                  className="button button-white"
+                  onClick={handleLanguageClick}
+                >
                   languages
                 </button>
               </div>
-              <div className='tech-skills grid'>
-                <img src={techSkills} alt='' />
-                <button className='button button-white' onClick={handleSkillsClick}>
+              <div className="tech-skills grid">
+                <img src={techSkills} alt="" />
+                <button
+                  className="button button-white"
+                  onClick={handleSkillsClick}
+                >
                   tech/soft skills
                 </button>
               </div>
             </div>
-            <div className='info-right'>
-              <div className='soft-skills grid'>
-                <img src={softSkills} alt='' />
-                <button className='button button-pink' onClick={handleProjectsClick}>
+            <div className="info-right">
+              <div className="soft-skills grid">
+                <img src={softSkills} alt="" />
+                <button
+                  className="button button-pink"
+                  onClick={handleProjectsClick}
+                >
                   projects
                 </button>
               </div>
-              <div className='certifications grid'>
-                <img src={certificate} alt='' />
-                <button className='button button-pink' onClick={handleCertificationClick}>
+              <div className="certifications grid">
+                <img src={certificate} alt="" />
+                <button
+                  className="button button-pink"
+                  onClick={handleCertificationClick}
+                >
                   certification
                 </button>
               </div>
             </div>
           </div>
-          <div className='recommendations'></div>
         </div>
+
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import "../css/footer.css";
+export default function Footer() {
+  return (
+    <footer>
+      <ol className="footer">
+        <li>+38097-608-96-80</li>
+        <li>
+          Telegram: 
+          <a
+            href="https://t.me/olha_kucheruk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             @olha_kucheruk
+          </a>
+        </li>
+      </ol>
+    </footer>
+  );
+}
